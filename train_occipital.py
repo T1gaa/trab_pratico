fd0c48ed9382d07d0c95882f8a80dfcf9dd3d567
João Silva <joaoluislg@gmail.com>
2024-10-30 15:02:35 +0000

    updated code of control dataset

--- train_occipital.py
+++ train_occipital.py
@@ -8,7 +8,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn import metrics
 
-
+"""
+Dados de controlo que são necessários ajustar para o checkpoint 1
+"""
 #Import dataset
 df = pd.read_csv('train_radiomics_occipital_CONTROL.csv')
 pd.set_option('display.max_columns',None)
@@ -16,6 +18,9 @@ pd.set_option('display.max_columns',None)
 
 #print(df.head())
 #print(df.dtypes)
-print(df.describe())
+#print(df.describe())
 
 #print(df.isna().any())
+
+#Dar drop a todas as colunas que são objetos excepto a coluna 'Transition'
+df_train.drop(df_train.select_dtypes(include='object').drop(columns=['Transition'], errors='ignore').columns, axis=1, inplace=True)
